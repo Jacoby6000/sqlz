22d06401c73ef33eb10f7ed224a1bb8704c6e6ae
Jacob Barber <Jacob.Barber@Bottlerocketstudios.com>
2016-02-02 12:07:02 -0600

    more updates! yeah!

--- src/main/scala/com/github/jacoby6000/sql/ast.scala
+++ src/main/scala/com/github/jacoby6000/sql/ast.scala
@@ -2,11 +2,28 @@ package com.github.jacoby6000.sql
 
 import com.github.jacoby6000.shapeless.proofs._
 import shapeless.HList
+import cats._
 
 /**
   * Created by jacob.barber on 2/2/16.
   */
 object ast {
+
+  trait ToSqlValue[A, B <: SqlValue] {
+    def sqlValueOf(a: A): B
+  }
+
+  implicit class ToSqlValueOps[A, B](a: A)(implicit toSqlValue: ToSqlValue[A, B]) {
+    def asSql = toSqlValue.sqlValueOf(a)
+  }
+
+  object ToSqlValue {
+    def apply[A, B <: SqlValue](f: A => B): ToSqlValue[A, B] = new ToSqlValue[A, B] {
+      def sqlValueOf(a: A): B = f(a)
+    }
+  }
+
+  type ToSqlNumber[A] = ToSqlValue[A, _ <: SqlNumber]
 
   sealed trait SqlValue
 
@@ -28,7 +45,6 @@ object ast {
 
   case class Insert[A <: HList, B <: HList](columns: A, values: B)(implicit columnsValuesSameLength: A SameLengthAs B) extends SqlAction
 
-
   case class Update[A <: HList, B <: HList](update: A, where: B)(implicit setsContainOnlyUpdateValues: A ContainsOnlyKind UpdateValue,
                                                                  whereClausesOnlyContainFilters: B ContainsOnlyKind SqlFilter) extends SqlAction
 
@@ -36,17 +52,22 @@ object ast {
 
 
   sealed trait SqlFilter[A]
-  case class In[A <: SqlValue](column: String, values: List[A]) extends SqlFilter[A]
-  case class Equal[A <: SqlValue](column: String, value: A) extends SqlFilter[A]
-  case class NotEqual[A <: SqlValue](column: String, value: A) extends SqlFilter[A]
-  case class LessThan[A <: SqlNumber](column: String, value: A) extends SqlFilter[A]
-  case class GreaterThan[A <: SqlNumber](column: String, value: A) extends SqlFilter[A]
-  case class LessThanOrEqual[A <: SqlNumber](column: String, value: A) extends SqlFilter[A]
-  case class GreaterThanOrEqual[A <: SqlNumber](column: String, value: A) extends SqlFilter[A]
-  case class Like[A <: SqlValue](column: String, value: A) extends SqlFilter[A]
-  case class Between[A <: SqlNumber](column: String, min: A, max: A) extends SqlFilter[A]
+  case class In[A](column: String, values: List[A]) extends SqlFilter[A]
+  case class Equal[A](column: String, value: A) extends SqlFilter[A]
+  case class NotEqual[A](column: String, value: A) extends SqlFilter[A]
+  case class LessThan[A: ToSqlNumber](column: String, value: A) extends SqlFilter[A]
+  case class GreaterThan[A: ToSqlNumber](column: String, value: A) extends SqlFilter[A]
+  case class LessThanOrEqual[A: ToSqlNumber](column: String, value: A) extends SqlFilter[A]
+  case class GreaterThanOrEqual[A: ToSqlNumber](column: String, value: A) extends SqlFilter[A]
+  case class Like[A](column: String, value: A) extends SqlFilter[A]
+  case class Between[A](column: String, min: A, max: A) extends SqlFilter[A]
 
-  case class UpdateValue[A <: SqlValue](column: String, value: A)
+  case class UpdateValue[A](column: String, value: A)
 
 
+  implicit val sqlStringToSqlValue = ToSqlValue { s: SqlString => s }
+  implicit val sqlIntToSqlValue = ToSqlValue { s: SqlInt => s }
+  implicit val sqlNumberToSqlValue = ToSqlValue { s: SqlNumber => s }
+  implicit val sqlDoubleToSqlValue = ToSqlValue { s: SqlDouble => s }
+  implicit val sqlBoolToSqlValue = ToSqlValue { s: SqlBoolean => s }
 }
