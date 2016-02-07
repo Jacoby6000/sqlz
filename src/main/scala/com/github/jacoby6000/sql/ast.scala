6eb38960afa803e9d5fb12f33335b75f9518470c
Jacob Barber <Jacob.Barber@Bottlerocketstudios.com>
2016-02-07 15:05:05 -0600

    General AST improvements, still need to work on select columns. Stuff is getting better, maybe... I Probably need to make the string interpreter less messy. It's a sea of spaghetti.

--- src/main/scala/com/github/jacoby6000/sql/ast.scala
+++ src/main/scala/com/github/jacoby6000/sql/ast.scala
@@ -5,6 +5,8 @@ import shapeless.HList
 import shapeless.ops.hlist.ToTraversable
 import shapeless.ops.tuple.ToList
 
+import scala.annotation.tailrec
+
 /**
   * Created by jacob.barber on 2/2/16.
   */
@@ -16,34 +18,65 @@ object ast {
   }
 
   sealed trait SqlValue {
-    def fold[B](string: String => B,
+    def fold[B](add: (SqlValue, SqlValue) => B,
+                sub: (SqlValue, SqlValue) => B,
+                div: (SqlValue, SqlValue) => B,
+                mul: (SqlValue, SqlValue) => B)
+               (literal: String => B,
+                string: String => B,
                 bool: Boolean => B,
                 int: Int => B,
                 double: Double => B,
-                prepared: => B): B = SqlValue.fold(this)(string, bool, int, double, prepared)
+                select: (List[String], List[SqlJoin], List[SqlFilter], List[String], Option[Int], Option[Int]) => B,
+                prepared: => B): B = SqlValue.fold(this)(add, sub, div, mul)(literal, string, bool, int, double, select, prepared)
   }
+  case class SqlLiteral(value: String) extends SqlValue
+
+  sealed trait SqlAlgebra extends SqlValue
+  case class SqlAdd(a: SqlValue, b: SqlValue) extends SqlAlgebra
+  case class SqlSub(a: SqlValue, b: SqlValue) extends SqlAlgebra
+  case class SqlDiv(a: SqlValue, b: SqlValue) extends SqlAlgebra
+  case class SqlMul(a: SqlValue, b: SqlValue) extends SqlAlgebra
+
+
+
   sealed trait SqlNumber extends SqlValue
   case object SqlPrepared extends SqlValue with SqlNumber
 
   case class SqlString(value: String) extends SqlValue
   case class SqlBoolean(value: Boolean) extends SqlValue
+  case class SqlSubQuery(query: Select) extends SqlValue
 
   case class SqlInt(value: Int) extends SqlNumber
   case class SqlDouble(value: Double) extends SqlNumber
 
   object SqlValue {
+
     def fold[B](sqlValue: SqlValue)
-               (string: String => B,
+               (add: (SqlValue, SqlValue) => B,
+                sub: (SqlValue, SqlValue) => B,
+                div: (SqlValue, SqlValue) => B,
+                mul: (SqlValue, SqlValue) => B)
+               (literal: String => B,
+                string: String => B,
                 bool: Boolean => B,
                 int: Int => B,
                 double: Double => B,
-                prepared: => B): B = sqlValue match {
-      case SqlString(s) => string(s)
-      case SqlBoolean(b) => bool(b)
-      case SqlInt(i) => int(i)
-      case SqlDouble(d) => double(d)
-      case SqlPrepared => prepared
-    }
+                select: (List[String], List[SqlJoin], List[SqlFilter], List[String], Option[Int], Option[Int]) => B,
+                prepared: => B): B =
+      sqlValue match {
+        case SqlAdd(a, b) => add(a,b)
+        case SqlMul(a, b) => mul(a,b)
+        case SqlDiv(a, b) => div(a,b)
+        case SqlSub(a, b) => sub(a,b)
+        case SqlString(s) => string(s)
+        case SqlBoolean(b) => bool(b)
+        case SqlInt(i) => int(i)
+        case SqlDouble(d) => double(d)
+        case SqlLiteral(l) => literal(l)
+        case SqlSubQuery(Select(columns, joins, where, groupBy, offset, limit)) => select(columns, joins, where, groupBy, offset, limit)
+        case SqlPrepared => prepared
+      }
   }
 
   sealed trait SqlJoin
@@ -54,21 +87,55 @@ object ast {
   case class CrossJoin(table: String, on: List[SqlFilter]) extends SqlJoin
 
 
-  sealed trait SqlFilter
-  case class In(column: String, values: List[SqlValue]) extends SqlFilter
-  case class Like(column: String, value: SqlValue) extends SqlFilter
-  case class Equal(column: String, value: SqlValue) extends SqlFilter
-  case class NotEqual(column: String, value: SqlValue) extends SqlFilter
-  case class LessThan(column: String, value: SqlValue) extends SqlFilter
-  case class GreaterThan(column: String, value: SqlValue) extends SqlFilter
-  case class LessThanOrEqual(column: String, value: SqlValue) extends SqlFilter
-  case class GreaterThanOrEqual(column: String, value: SqlValue) extends SqlFilter
-  case class Between(column: String, min: SqlValue, max: SqlValue) extends SqlFilter
+  sealed trait SqlFilter {
+    def fold[B](in: (SqlValue, List[SqlValue]) => B,
+                like: (SqlValue, SqlValue) => B,
+                equal: (SqlValue, SqlValue) => B,
+                notEqual: (SqlValue, SqlValue) => B,
+                lessThan: (SqlValue, SqlValue) => B,
+                greaterThan: (SqlValue, SqlValue) => B,
+                lessThanOrEqual: (SqlValue, SqlValue) => B,
+                greaterThanOrEqual: (SqlValue, SqlValue) => B,
+                between: (SqlValue, SqlValue, SqlValue) => B): B = SqlFilter.fold(this)(in, like, equal, notEqual, lessThan, greaterThan, lessThanOrEqual, greaterThanOrEqual, between)
+  }
+  case class In(column: SqlValue, values: List[SqlValue]) extends SqlFilter
+  case class Like(column: SqlValue, value: SqlValue) extends SqlFilter
+  case class Equal(column: SqlValue, value: SqlValue) extends SqlFilter
+  case class NotEqual(column: SqlValue, value: SqlValue) extends SqlFilter
+  case class LessThan(column: SqlValue, value: SqlValue) extends SqlFilter
+  case class GreaterThan(column: SqlValue, value: SqlValue) extends SqlFilter
+  case class LessThanOrEqual(column: SqlValue, value: SqlValue) extends SqlFilter
+  case class GreaterThanOrEqual(column: SqlValue, value: SqlValue) extends SqlFilter
+  case class Between(column: SqlValue, min: SqlValue, max: SqlValue) extends SqlFilter
+
+  object SqlFilter {
+    def fold[B](filter: SqlFilter)
+               (in: (SqlValue, List[SqlValue]) => B,
+                like: (SqlValue, SqlValue) => B,
+                equal: (SqlValue, SqlValue) => B,
+                notEqual: (SqlValue, SqlValue) => B,
+                lessThan: (SqlValue, SqlValue) => B,
+                greaterThan: (SqlValue, SqlValue) => B,
+                lessThanOrEqual: (SqlValue, SqlValue) => B,
+                greaterThanOrEqual: (SqlValue, SqlValue) => B,
+                between: (SqlValue, SqlValue, SqlValue) => B): B =
+      filter match {
+        case In(column, values) => in(column, values)
+        case Like(column, value) => like(column, value)
+        case Equal(column, value) => equal(column, value)
+        case NotEqual(column, value) => notEqual(column, value)
+        case LessThan(column, value) => lessThan(column, value)
+        case GreaterThan(column, value) => greaterThan(column, value)
+        case LessThanOrEqual(column, value) => lessThanOrEqual(column, value)
+        case GreaterThanOrEqual(column, value) => greaterThanOrEqual(column, value)
+        case Between(column, min, max) => between(column, min, max)
+      }
+  }
 
   case class UpdateValue(column: String, value: SqlValue)
 
   sealed trait SqlAction {
-    def fold[B](select: (List[String], List[SqlJoin], List[SqlFilter], List[String], Int, Int) => B,
+    def fold[B](select: (List[String], List[SqlJoin], List[SqlFilter], List[String], Option[Int], Option[Int]) => B,
                 update: (List[UpdateValue], List[SqlFilter]) => B,
                 insert: (List[String], List[SqlValue]) => B,
                 bulkInsert: (List[String], List[List[SqlValue]]) => B,
@@ -107,12 +174,12 @@ object ast {
                                                  (implicit hUpdateContainOnlyUpdateValues: A ContainsOnly UpdateValue,
                                                            hWhereOnlyContainFilters: B ContainsOnly SqlFilter,
                                                            hUpdateToList: ToTraversable.Aux[A, List, UpdateValue],
-                                                           hWhereToList: ToTraversable.Aux[B, List, SqlFilter]) = new Update(hUpdate.toList, hWhere.toList)
+                                                           hWhereToList: ToTraversable.Aux[B, List, SqlFilter]): Update = Update(hUpdate.toList, hWhere.toList)
   }
 
-  case class Select(columns: List[String], joins: List[SqlJoin], where: List[SqlFilter], groupBy: List[String], offset: Int, limit: Int) extends SqlAction
+  case class Select(columns: List[String], joins: List[SqlJoin], where: List[SqlFilter], groupBy: List[String], offset: Option[Int], limit: Option[Int]) extends SqlAction
   object Select {
-    def apply[A <: HList, B <: HList, C <: HList, D <: HList](hColumns: A, hJoins: B, hWhere: C, hGroupBy: D, offset: Int, limit: Int)
+    def apply[A <: HList, B <: HList, C <: HList, D <: HList](hColumns: A, hJoins: B, hWhere: C, hGroupBy: D, offset: Option[Int], limit: Option[Int])
                                                              (implicit hColumnsContainsOnlyStrings: A ContainsOnly String,
                                                                        hJoinsContainsOnlySqlJoin: B ContainsOnly SqlJoin,
                                                                        hWhereContainsOnlySqlFilter: C ContainsOnly SqlFilter,
@@ -127,22 +194,23 @@ object ast {
 
   object Delete {
     def apply[A <: HList](hWhere: A)(implicit hWhereContainsOnlySqlFilter: A ContainsOnly SqlFilter,
-                                              hWhereToList: ToTraversable.Aux[A, List, SqlFilter]) = Delete(hWhere.toList)
+                                              hWhereToList: ToTraversable.Aux[A, List, SqlFilter]): Delete = Delete(hWhere.toList)
   }
 
   object SqlAction {
     def fold[B](action: SqlAction)
-               (select: (List[String], List[SqlJoin], List[SqlFilter], List[String], Int, Int) => B,
+               (select: (List[String], List[SqlJoin], List[SqlFilter], List[String], Option[Int], Option[Int]) => B,
                 update: (List[UpdateValue], List[SqlFilter]) => B,
                 insert: (List[String], List[SqlValue]) => B,
                 bulkInsert: (List[String], List[List[SqlValue]]) => B,
-                delete: List[SqlFilter] => B): B = action match {
-      case Select(columns, joins, where, groupBy, offset, limit) => select(columns, joins, where, groupBy, offset, limit)
-      case Update(values, where) => update(values, where)
-      case Insert(columns, values) => insert(columns, values)
-      case BulkInsert(columns, values) => bulkInsert(columns, values)
-      case Delete(where) => delete(where)
-    }
-
+                delete: List[SqlFilter] => B): B =
+      action match {
+        case Select(columns, joins, where, groupBy, offset, limit) => select(columns, joins, where, groupBy, offset, limit)
+        case Update(values, where) => update(values, where)
+        case Insert(columns, values) => insert(columns, values)
+        case BulkInsert(columns, values) => bulkInsert(columns, values)
+        case Delete(where) => delete(where)
+      }
   }
+
 }
--- src/main/scala/com/github/jacoby6000/sql/interpreters.scala
+++ src/main/scala/com/github/jacoby6000/sql/interpreters.scala
@@ -8,12 +8,68 @@ import com.github.jacoby6000.sql.ast._
   */
 object interpreters {
   def interpretToString(action: DatabaseAction): String = {
+    val table = action.table
+
+    def selectToString(columns: List[String], joins: List[SqlJoin], where: List[SqlFilter], groupBy: List[String], offset: Option[Int], limit: Option[Int]): String = {
+      val baseSelect = s"SELECT ${columns.mkString(", ")} FROM $table"
+      val whereString = where.map(filterToSqlString).mkString("WHERE ", " AND\n", "")
+      val joinString = joins.map {
+        case FullOuterJoin(otherTable, joinFilters) => buildJoin("FULL OUTER JOIN", otherTable, joinFilters)
+        case LeftOuterJoin(otherTable, joinFilters) => buildJoin("LEFT JOIN", otherTable, joinFilters)
+        case RightOuterJoin(otherTable, joinFilters) => buildJoin("RIGHT JOIN", otherTable, joinFilters)
+        case InnerJoin(otherTable, joinFilters) => buildJoin("INNER JOIN", otherTable, joinFilters)
+        case CrossJoin(otherTable, joinFilters) => buildJoin("CROSS JOIN", otherTable, joinFilters)
+      }.mkString("\n")
+
+      val groupByString = groupBy.mkString(", ")
+      val offsetString = offset.map("SKIP " + _) getOrElse ""
+      val limitString = limit.map("LIMIT " + _) getOrElse ""
+
+      s"$baseSelect\n$joinString\n$whereString\n$offsetString\n$limitString"
+    }
+
+    def buildStandardOperation(operator: String): (SqlValue, SqlValue) => String = (s, v) => s"${sqlValueToSqlString(s)} $operator ${sqlValueToSqlString(v)}"
+
+
+    def updateWithNoFilter(list: List[UpdateValue]) = s"UPDATE $table SET ${list.map(v => v.column + "=" + sqlValueToSqlString(v.value))}"
+
     def sqlValueToSqlString(sqlValue: SqlValue): String =
-      sqlValue.fold(_.surround('"'), _.toString, _.toString, _.toString, "?")
+      sqlValue.fold(
+        add = (a,b) => sqlValueToSqlString(a) + " + " + sqlValueToSqlString(b),
+        sub = (a,b) => sqlValueToSqlString(a) + " - " + sqlValueToSqlString(b),
+        div = (a,b) => sqlValueToSqlString(a) + " / " + sqlValueToSqlString(b),
+        mul = (a,b) => sqlValueToSqlString(a) + " * " + sqlValueToSqlString(b)
+      )(identity, _.surround('"'), _.toString, _.toString, _.toString, selectToString, "?")
+
+    def buildJoin(joinType: String, tbl: String, filters: List[SqlFilter]): String = s"$joinType $tbl ON " + filters.map(filterToSqlString).mkString(" AND\n")
+
+    def filterToSqlString(filter: SqlFilter): String =
+      filter.fold(like               = buildStandardOperation("LIKE"),
+                  equal              = buildStandardOperation("="),
+                  notEqual           = buildStandardOperation("<>"),
+                  lessThan           = buildStandardOperation("<"),
+                  greaterThan        = buildStandardOperation(">"),
+                  lessThanOrEqual    = buildStandardOperation("<="),
+                  greaterThanOrEqual = buildStandardOperation(">="),
+                  in = (col, values) => s"${sqlValueToSqlString(col)} in (${values.map(sqlValueToSqlString).mkString(", ")})",
+                  between =  (col, min, max) => s"${sqlValueToSqlString(col)} BETWEEN ${sqlValueToSqlString(min)} AND ${sqlValueToSqlString(max)}")
 
     action.run match {
       case Insert(columns, values) =>
-        s"insert into ${action.table} (${columns.mkString(",\n")}) values (${values.map(sqlValueToSqlString).mkString(",\n")})"
+        s"INSERT INTO $table (${columns.mkString(",\n")}) values (${values.map(sqlValueToSqlString).mkString(",\n")})"
+
+      case Select(cols, joins, where, group, offset, limit) =>
+        selectToString(cols, joins, where, group, offset, limit)
+
+      case Update(updates, Nil) =>
+        updateWithNoFilter(updates)
+
+      case Update(updates, filters) =>
+        updateWithNoFilter(updates) + "WHERE " + filters.map(filterToSqlString).mkString(" AND ")
+
+      case Delete(where) => s"DELETE FROM $table WHERE ${where.map(filterToSqlString).mkString(" AND ")}"
+
+      case BulkInsert(_, _) => "" // TODO: Implement me
     }
   }
 
