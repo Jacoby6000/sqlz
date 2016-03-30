05154315979391d129d392eeb0b21cd6edd0c695
Jacob Barber <JacobA.Barber@yahoo.com>
2016-03-30 08:12:20 -0500

    made "narrow" in to an extension method "narrowed"

--- src/main/scala/com/github/jacoby6000/query/doobie.scala
+++ src/main/scala/com/github/jacoby6000/query/doobie.scala
@@ -14,7 +14,9 @@ object doobie {
   implicit def selectBuilderToSelectQuery(queryBuilder: QueryBuilder): DoobieExpressionExtensions = new DoobieExpressionExtensions(queryBuilder.query)
   implicit def updateBuilderToUpdateQuery(updateBuilder: UpdateBuilder): DoobieExpressionExtensions = new DoobieExpressionExtensions(updateBuilder.query)
 
-  def narrow[A <: AnyRef](a: A): a.type {} = a
+  implicit class NarrowingExtensions[A <: AnyRef](val a: A) extends AnyVal{
+    def narrowed: a.type {} = a
+  }
 
   implicit class DoobieExpressionExtensions(val query: QueryExpression) extends AnyVal {
     def sql: String = interpreter.interpretPSql(query)
