87ec89c9aab6aa7d0cc1a4723189b52a1da69655
Jacob Barber <Jacob.Barber@Bottlerocketstudios.com>
2016-05-10 12:29:35 -0500

    It's alive!

--- src/main/scala/com/github/jacoby6000/query/ast.scala
+++ src/main/scala/com/github/jacoby6000/query/ast.scala
@@ -1,7 +1,8 @@
 package com.github.jacoby6000.query
 import doobie.imports._
-import shapeless._
-import shapeless.ops.hlist.{Comapped, LeftFolder, Mapped, Mapper, Prepend}
+import _root_.shapeless._
+import _root_.shapeless.ops.hlist.{Mapper, Prepend, ToTraversable}
+import com.github.jacoby6000.query.shapeless.KindConstraint.ConstrainedUnaryTCConstraintTC
 
 
 /**
@@ -9,29 +10,38 @@ import shapeless.ops.hlist.{Comapped, LeftFolder, Mapped, Mapper, Prepend}
   */
 object ast {
 
-  trait OfKindContainingHListTC[L <: HList, TC[_ <: HList]] extends Serializable
-
-  object OfKindContainingHListTC {
-    def apply[L <: HList, TC[_ <: HList]](implicit utcc: OfKindContainingHListTC[L, TC]): OfKindContainingHListTC[L, TC] = utcc
-
+  private object OfKindContainingHListTC extends ConstrainedUnaryTCConstraintTC[HList] {
     type OfKindContainingHList[TC[_ <: HList]] = {
-      type HL[L <: HList] = OfKindContainingHListTC[L, TC]
+      type HL[L <: HList] = ConstrainedUnaryTCConstraintTC[L, TC]
     }
-
-    implicit def hnilUnaryTC[TC[_ <: HList]] = new OfKindContainingHListTC[HNil, TC] {}
-    implicit def hlistUnaryTC1[H <: HList, T <: HList, TC[_ <: HList]](implicit utct : OfKindContainingHListTC[T, TC]) =
-      new OfKindContainingHListTC[TC[H] :: T, TC] {}
-
-    implicit def hlistUnaryTC2[L <: HList] = new OfKindContainingHListTC[L, Id] {}
-
-    implicit def hlistUnaryTC3[H] = new OfKindContainingHListTC[HNil, Const[H]#λ] {}
-    implicit def hlistUnaryTC4[H, T <: HList](implicit utct : OfKindContainingHListTC[T, Const[H]#λ]) =
-      new OfKindContainingHListTC[H :: T, Const[H]#λ] {}
   }
 
-  import OfKindContainingHListTC.OfKindContainingHList
+  import OfKindContainingHListTC._
 
+  trait UnwrapperPoly[F[_ <: HList]] extends Poly1 {
+    implicit def unwrapHValue[A <: HList] = at[F[A]](unwrap)
+    def unwrap[A <: HList](f: F[A]): A
+  }
 
+  object QueryProjectionUnwrapper extends UnwrapperPoly[QueryProjection] {
+    def unwrap[A <: HList](f: QueryProjection[A]): A = f.params
+  }
+
+  object QueryUnionUnwrapper extends UnwrapperPoly[QueryUnion] {
+    def unwrap[A <: HList](f: QueryUnion[A]): A = f.params
+  }
+
+  object QueryComparisonUnwrapper extends UnwrapperPoly[QueryComparison] {
+    def unwrap[A <: HList](f: QueryComparison[A]): A = f.params
+  }
+
+  object ModifyFieldUnwrapper extends UnwrapperPoly[ModifyField] {
+    def unwrap[A <: HList](f: ModifyField[A]): A = f.params
+  }
+
+  object QueryValueUnwrapper extends UnwrapperPoly[QueryValue] {
+    def unwrap[A <: HList](f: QueryValue[A]): A = f.params
+  }
 
   trait RawExpressionHandler[A] {
     def interpret(a: A): String
@@ -43,10 +53,18 @@ object ast {
     }
   }
 
-  sealed trait QueryValue[L <: HList] { def params: L }
+  object QueryFunction {
+    def apply[L <: HList : OfKindContainingHList[QueryValue]#HL, Out <: HList](path: QueryPath, args: L)(implicit toList: ToTraversable.Aux[L, List, QueryValue[_]], m: Mapper.Aux[QueryValueUnwrapper.type, L, Out]): QueryFunction[Out] = QueryFunction[Out](path, toList(args), m(args))
+  }
+
+  object QueryIn {
+    def apply[A <: HList, B <: HList : OfKindContainingHList[QueryValue]#HL, MappedValues <: HList, Out <: HList](left: QueryValue[A], rights: B)(implicit toList: ToTraversable.Aux[B, List, QueryValue[_]], m: Mapper.Aux[QueryValueUnwrapper.type, B, MappedValues], p: Prepend.Aux[A,MappedValues,Out]): QueryIn[A,Out] = QueryIn[A,Out](left, toList(rights), left.params ::: m(rights))
+  }
+
+  sealed trait QueryValue[+L <: HList] { def params: L }
   case class QueryRawExpression[T](t: T)(implicit val rawExpressionHandler: RawExpressionHandler[T]) extends QueryValue[HNil] { lazy val params: HNil = HNil }
   case class QueryParameter[T](value: T) extends QueryValue[T :: HNil] { lazy val params: T :: HNil = value :: HNil}
-  case class QueryFunction[L <: HList](path: QueryPath, args: L) extends QueryValue[L] { lazy val params: L = args }
+  case class QueryFunction[L <: HList] private (path: QueryPath, args: List[QueryValue[_]], params: L) extends QueryValue[L]
   case class QueryAdd[A <: HList, B <: HList, Out <: HList](left: QueryValue[A], right: QueryValue[B])(implicit p: Prepend.Aux[A,B,Out]) extends QueryValue[Out] { lazy val params: Out = left.params ::: right.params }
   case class QuerySub[A <: HList, B <: HList, Out <: HList](left: QueryValue[A], right: QueryValue[B])(implicit p: Prepend.Aux[A,B,Out]) extends QueryValue[Out] { lazy val params: Out = left.params ::: right.params }
   case class QueryDiv[A <: HList, B <: HList, Out <: HList](left: QueryValue[A], right: QueryValue[B])(implicit p: Prepend.Aux[A,B,Out]) extends QueryValue[Out] { lazy val params: Out = left.params ::: right.params }
@@ -60,11 +78,11 @@ object ast {
   case class QueryGreaterThanOrEqual[A <: HList, B <: HList, Out <: HList](left: QueryValue[A], right: QueryValue[B])(implicit p: Prepend.Aux[A,B,Out]) extends QueryComparison[Out] { lazy val params: Out = left.params ::: right.params }
   case class QueryLessThan[A <: HList, B <: HList, Out <: HList](left: QueryValue[A], right: QueryValue[B])(implicit p: Prepend.Aux[A,B,Out]) extends QueryComparison[Out] { lazy val params: Out = left.params ::: right.params }
   case class QueryLessThanOrEqual[A <: HList, B <: HList, Out <: HList](left: QueryValue[A], right: QueryValue[B])(implicit p: Prepend.Aux[A,B,Out]) extends QueryComparison[Out] { lazy val params: Out = left.params ::: right.params }
-//  case class QueryIn[A <: HList, B <: HList : OfKind[QueryValue]#HL](left: QueryValue[A], rights: B) extends QueryComparison
+  case class QueryIn[A <: HList, B <: HList] private (left: QueryValue[A], rights: List[QueryValue[_]], params: B) extends QueryComparison[B]
   case class QueryLit[A <: HList](value: QueryValue[A]) extends QueryComparison[A] { lazy val params: A = value.params }
   case class QueryNot[A <: HList](value: QueryComparison[A]) extends QueryComparison[A] { lazy val params: A = value.params }
-  case class QueryAnd[A <: HList, B <: HList, Out <: HList](left: QueryValue[A], right: QueryValue[B])(implicit p: Prepend.Aux[A,B,Out]) extends QueryComparison[Out] { lazy val params: Out = left.params ::: right.params }
-  case class QueryOr[A <: HList, B <: HList, Out <: HList](left: QueryValue[A], right: QueryValue[B])(implicit p: Prepend.Aux[A,B,Out]) extends QueryComparison[Out] { lazy val params: Out = left.params ::: right.params }
+  case class QueryAnd[A <: HList, B <: HList, Out <: HList](left: QueryComparison[A], right: QueryComparison[B])(implicit p: Prepend.Aux[A,B,Out]) extends QueryComparison[Out] { lazy val params: Out = left.params ::: right.params }
+  case class QueryOr[A <: HList, B <: HList, Out <: HList](left: QueryComparison[A], right: QueryComparison[B])(implicit p: Prepend.Aux[A,B,Out]) extends QueryComparison[Out] { lazy val params: Out = left.params ::: right.params }
 
   sealed trait QueryPath extends QueryValue[HNil] { lazy val params: HNil = HNil }
   case class QueryPathEnd(path: String) extends QueryPath with QueryValue[HNil]
@@ -88,83 +106,58 @@ object ast {
   sealed trait QueryExpression[A <: HList] { def params: A }
   sealed trait QueryModify[A <: HList] extends QueryExpression[A]
 
-
-  trait UnwrapperPoly[F[_ <: HList]] extends Poly1 {
-    implicit def unwrapHValue[A <: HList] = at[F[A]](unwrap)
-    def unwrap[A <: HList](f: F[A]): A
-  }
-
-  object QueryProjectionUnwrapper extends UnwrapperPoly[QueryProjection] {
-    def unwrap[A <: HList](f: QueryProjection[A]): A = f.params
-  }
-
-  object QueryUnionUnwrapper extends UnwrapperPoly[QueryUnion] {
-    def unwrap[A <: HList](f: QueryUnion[A]): A = f.params
-  }
-
-  object QueryComparisonUnwrapper extends UnwrapperPoly[QueryComparison] {
-    def unwrap[A <: HList](f: QueryComparison[A]): A = f.params
-  }
-
-  object ModifyFieldUnwrapper extends UnwrapperPoly[ModifyField] {
-    def unwrap[A <: HList](f: ModifyField[A]): A = f.params
-  }
-
-
   object QuerySelect {
     def apply[
-      Table <: HList,
-      QueryProjections <: HList : OfKindContainingHList[QueryProjection]#HL,
-      QueryUnions <: HList : OfKindContainingHList[QueryUnion]#HL,
-      QueryComparisons <: HList,
-      MappedValues <: HList,
-      MappedUnions <: HList,
-      MappedFilters <: HList,
-      Out1 <: HList,
-      Out2 <: HList,
-      Params <: HList
-      ](
-         table: QueryProjection[Table],
-         values: QueryProjections,
-         unions: QueryUnions,
-         filter: QueryComparison[QueryComparisons],
-         sorts: List[QuerySort],
-         groupings: List[QuerySort],
-         offset: Option[Int],
-         limit: Option[Int]
-       )(implicit
-         mv: Mapper.Aux[QueryProjectionUnwrapper.type, QueryProjections, MappedValues],
-         mu: Mapper.Aux[QueryUnionUnwrapper.type, QueryUnions, MappedUnions],
-         p1: Prepend.Aux[Table, MappedValues, Out1],
-         p2: Prepend.Aux[Out1, MappedUnions, Out2],
-         p3: Prepend.Aux[Out2, QueryComparisons, Params]
-       ): QuerySelect[Table, QueryProjections, QueryUnions, QueryComparisons, Params] = QuerySelect(table, values, unions, filter, sorts, groupings, offset, limit, p3(p2(p1(table.params,mv(values)),mu(unions)), filter.params))
+            Table <: HList,
+            QueryProjections <: HList : OfKindContainingHList[QueryProjection]#HL,
+            QueryUnions <: HList : OfKindContainingHList[QueryUnion]#HL,
+            ComparisonParameters <: HList,
+            MappedValues <: HList,
+            MappedUnions <: HList,
+            MappedFilters <: HList,
+            Out1 <: HList,
+            Out2 <: HList,
+            Params <: HList
+          ](
+            table: QueryProjection[Table],
+            values: QueryProjections,
+            unions: QueryUnions,
+            filter: QueryComparison[ComparisonParameters],
+            sorts: List[QuerySort],
+            groupings: List[QuerySort],
+            offset: Option[Int],
+            limit: Option[Int]
+          )(implicit
+            mv: Mapper.Aux[QueryProjectionUnwrapper.type, QueryProjections, MappedValues],
+            mu: Mapper.Aux[QueryUnionUnwrapper.type, QueryUnions, MappedUnions],
+            p1: Prepend.Aux[Table, MappedValues, Out1],
+            p2: Prepend.Aux[Out1, MappedUnions, Out2],
+            p3: Prepend.Aux[Out2, ComparisonParameters, Params],
+            pl: ToTraversable.Aux[QueryProjections, List, QueryProjection[_ <: HList]],
+            ul: ToTraversable.Aux[QueryUnions, List, QueryUnion[_ <: HList]]
+          ): QuerySelect[Params] =
+              QuerySelect[Params](table, pl(values), ul(unions), filter, sorts, groupings, offset, limit, p3(p2(p1(table.params,mv(values)),mu(unions)), filter.params))
   }
 
-  case class QuerySelect[
-                          Table <: HList,
-                          QueryProjections <: HList : OfKindContainingHList[QueryProjection]#HL,
-                          QueryUnions <: HList : OfKindContainingHList[QueryUnion]#HL,
-                          QueryComparisons <: HList,
-                          Params <: HList
-                        ] private (
-                          table: QueryProjection[Table],
-                          values: QueryProjections,
-                          unions: QueryUnions,
-                          filter: QueryComparison[QueryComparisons],
+  case class QuerySelect[Params <: HList] private (
+                          table: QueryProjection[_ <: HList],
+                          values: List[QueryProjection[_ <: HList]],
+                          unions: List[QueryUnion[_ <: HList]],
+                          filter: QueryComparison[_ <: HList],
                           sorts: List[QuerySort],
                           groupings: List[QuerySort],
                           offset: Option[Int],
                           limit: Option[Int],
                           params: Params
-                        ) extends QueryExpression[Params]
+                        ) extends QueryExpression[Params] with QueryValue[Params]
 
   case class ModifyField[A <: HList](key: QueryPath, value: QueryValue[A]) { lazy val params: A = value.params}
-  case class QueryInsert[A <: HList : OfKindContainingHList[ModifyField]#HL, Out <: HList]
-                        (collection: QueryPath, values: A)
-                        (implicit m: Mapper.Aux[ModifyFieldUnwrapper.type, A, Out]) extends QueryExpression[Out] with QueryModify[Out] {
-    lazy val params: Out = m(values)
-  }
+//  case class QueryInsert[A <: HList : OfKindContainingHList[ModifyField]#HL, Out <: HList]
+//                        (collection: QueryPath, values: A)
+//                        (implicit m: Mapper.Aux[ModifyFieldUnwrapper.type, A, Out]) extends QueryExpression[Out] with QueryModify[Out] {
+//    lazy val params: Out = m(values)
+//  }
+
 //  case class QueryUpdate[A <: HList : OfKind[ModifyField]#HL, B <: HList](collection: QueryPath, values: A, where: Option[B]) extends QueryExpression[B] with QueryModify[B]
 //  case class QueryDelete[B <:(collection: QueryPath, where: QueryComparison) extends QueryExpression
 
--- src/main/scala/com/github/jacoby6000/query/interpreters.scala
+++ src/main/scala/com/github/jacoby6000/query/interpreters.scala
@@ -1,105 +1,115 @@
-//package com.github.jacoby6000.query
-//
-//import com.github.jacoby6000.query.ast._
-//import doobie.imports.{Param, Query0}
-//import doobie.util.composite.Composite
-//import doobie.util.query.Query
-//
-///**
-//  * Created by jacob.barber on 3/3/16.
-//  */
-//object interpreters {
-//  class SqlInterpreter {
-//    def query[A : Param, B: Composite](ast: QueryExpression[A]): Query0[B] =
-//      Query[A,B](interpretPSql(ast), None).toQuery0(ast.params)
-//  }
-//
-//  object sqlDialects {
-//    implicit val postgres = new SqlInterpreter(interpretPSql _)
-//  }
-//
-//  def interpretPSql(expr: QueryExpression[_]): String = {
-//    val singleQuote = '"'.toString
-//    def wrap(s: String, using: String): String = s"$using$s$using"
-//
-//    def binOpReduction[A](op: String, left: A, right: A)(f: A => String) = f(left) + wrap(op, " ") + f(right)
-//
-//    def reducePath(queryPath: QueryPath): String = queryPath match {
-//      case QueryPathEnd(str) => wrap(str, singleQuote)
-//      case QueryPathCons(head, tail) => wrap(head, singleQuote) + "." + reducePath(tail)
-//    }
-//
-//    def reduceProjection(projection: QueryProjection[_]): String = projection match {
-//      case QueryProjectAll => "*"
-//      case QueryProjectOne(path, alias) => reduceValue(path) + alias.map(" AS " + _).getOrElse("")
-//    }
-//
-//    def reduceValue(value: QueryValue[_]): String = value match {
-//      case expr @ QueryRawExpression(ex) => expr.rawExpressionHandler.interpret(ex)
-//      case QueryParameter(s) => "?"
-//      case QueryPathCons(a,b) => reducePath(QueryPathCons(a,b))
-//      case QueryPathEnd(a) => reducePath(QueryPathEnd(a))
-//      case QueryFunction(path, args) => reducePath(path) + "(" + args.map(reduceValue).mkString(", ") + ")"
-//      case QueryAdd(left, right) => binOpReduction("+", left, right)(reduceValue)
-//      case QuerySub(left, right) => binOpReduction("-", left, right)(reduceValue)
-//      case QueryDiv(left, right) => binOpReduction("/", left, right)(reduceValue)
-//      case QueryMul(left, right) => binOpReduction("*", left, right)(reduceValue)
-//      case sel: QuerySelect => "(" + interpretPSql(sel) + ")"
-//      case QueryNull => "NULL"
-//    }
-//
-//    def reduceComparison(value: QueryComparison[_]): String = value match {
-//      case QueryLit(v) => reduceValue(v)
-//      case QueryEqual(left, QueryNull) => reduceValue(left) + " IS NULL"
-//      case QueryEqual(left, right) => binOpReduction("=", left, right)(reduceValue)
-//      case QueryNotEqual(left, QueryNull) => reduceValue(left) + " IS NOT NULL"
-//      case QueryNotEqual(left, right) => binOpReduction("<>", left, right)(reduceValue)
-//      case QueryGreaterThan(left, right) => binOpReduction(">", left, right)(reduceValue)
-//      case QueryGreaterThanOrEqual(left, right) => binOpReduction(">=", left, right)(reduceValue)
-////      case QueryIn(left, rights) => reduceValue(left) + " IN " + rights.map(reduceValue).mkString("(", ", ", ")")
-//      case QueryLessThan(left, right) => binOpReduction("<", left, right)(reduceValue)
-//      case QueryLessThanOrEqual(left, right) => binOpReduction("<=", left, right)(reduceValue)
-//      case QueryAnd(left, right) => binOpReduction(" AND ", left, right)(reduceComparison)
-//      case QueryOr(left, right) => binOpReduction(" OR ", left, right)(reduceComparison)
-//      case QueryNot(v) => "!" + reduceComparison(v)
-//    }
-//
-//    def reduceUnion(union: QueryUnion[_]): String = union match {
-//      case QueryLeftOuterJoin(path, logic) => "LEFT OUTER JOIN " + reduceProjection(path) + " ON " + reduceComparison(logic)
-//      case QueryRightOuterJoin(path, logic) => "RIGHT OUTER JOIN " + reduceProjection(path) + " ON " + reduceComparison(logic)
-//      case QueryCrossJoin(path, logic) => "CROSS JOIN " + reduceProjection(path) + " ON " + reduceComparison(logic)
-//      case QueryFullOuterJoin(path, logic) => "FULL OUTER JOIN " + reduceProjection(path) + " ON " + reduceComparison(logic)
-//      case QueryInnerJoin(path, logic) => "INNER JOIN " + reduceProjection(path) + " ON " + reduceComparison(logic)
-//    }
-//
-//    def reduceSort(sort: QuerySort): String = sort match {
-//      case QuerySortAsc(path) => reducePath(path) + " ASC"
-//      case QuerySortDesc(path) => reducePath(path) + " DESC"
-//    }
-//
-//    def reduceInsertValues(insertValue: ModifyField[_]): (String, String) =
-//      reducePath(insertValue.key) -> reduceValue(insertValue.value)
-//
-//    expr match {
-//      case QuerySelect(table, values, unions, filters, sorts, groups, offset, limit) =>
-//        val sqlProjections = values.map(reduceProjection).mkString(", ")
-//        val sqlFilter = "WHERE " + reduceComparison(filters)
-//        val sqlUnions = unions.map(reduceUnion).mkString(" ")
-//        val sqlSorts =
-//          if(sorts.isEmpty) ""
-//          else "ORDER BY " + sorts.map(reduceSort).mkString(", ")
-//
-//        val sqlGroups =
-//          if(groups.isEmpty) ""
-//          else "GROUP BY " + groups.map(reduceSort).mkString(", ")
-//
-//        val sqlTable = reduceProjection(table)
-//
-//        val sqlOffset = offset.map("OFFSET " + _).getOrElse("")
-//        val sqlLimit = limit.map("LIMIT " + _).getOrElse("")
-//
-//        s"SELECT $sqlProjections FROM $sqlTable $sqlUnions $sqlFilter $sqlSorts $sqlGroups $sqlLimit $sqlOffset".trim
-//
+package com.github.jacoby6000.query
+
+import com.github.jacoby6000.query.ast._
+import doobie.imports._
+import _root_.shapeless._
+import _root_.shapeless.ops.hlist.ToTraversable
+
+
+/**
+  * Created by jacob.barber on 3/3/16.
+  */
+object interpreters {
+  def void[A](a: A): Unit = ()
+
+  case class SqlInterpreter(genSql: QueryExpression[_] => String) {
+    def query[A <: HList : Param, B: Composite](ast: QuerySelect[A], printer: String => Unit = void): Query0[B] = {
+      val sql = genSql(ast)
+      printer(sql)
+      Query[A, B](sql, None)(implicitly[Param[A]].composite, implicitly[Composite[B]]).toQuery0(ast.params)
+    }
+  }
+
+  implicit class QuerySelectExtensions[A <: HList : Param](expr: QuerySelect[A])(implicit sqlInterpreter: SqlInterpreter) {
+    def query[B: Composite](printer: String => Unit = void): Query0[B] = sqlInterpreter.query[A,B](expr, printer)
+  }
+
+  object sqlDialects {
+    implicit val postgres = SqlInterpreter(interpretPSql _)
+  }
+
+  def interpretPSql(expr: QueryExpression[_]): String = {
+    val singleQuote = '"'.toString
+    def wrap(s: String, using: String): String = s"$using$s$using"
+
+    def binOpReduction[A](op: String, left: A, right: A)(f: A => String) = f(left) + wrap(op, " ") + f(right)
+
+    def reducePath(queryPath: QueryPath): String = queryPath match {
+      case QueryPathEnd(str) => wrap(str, "")
+      case QueryPathCons(head, tail) => wrap(head, "") + "." + reducePath(tail)
+    }
+
+    def reduceProjection(projection: QueryProjection[_]): String = projection match {
+      case QueryProjectAll => "*"
+      case QueryProjectOne(path, alias) => reduceValue(path) + alias.map(" AS " + _).getOrElse("")
+    }
+
+    def reduceValue(value: QueryValue[_]): String = value match {
+      case expr @ QueryRawExpression(ex) => expr.rawExpressionHandler.interpret(ex)
+      case QueryParameter(s) => "?"
+      case QueryPathCons(a,b) => reducePath(QueryPathCons(a,b))
+      case QueryPathEnd(a) => reducePath(QueryPathEnd(a))
+      case QueryFunction(path, args, _) => reducePath(path) + "(" + args.map(reduceValue).mkString(", ") + ")"
+      case QueryAdd(left, right) => binOpReduction("+", left, right)(reduceValue)
+      case QuerySub(left, right) => binOpReduction("-", left, right)(reduceValue)
+      case QueryDiv(left, right) => binOpReduction("/", left, right)(reduceValue)
+      case QueryMul(left, right) => binOpReduction("*", left, right)(reduceValue)
+      case sel: QuerySelect[_] => "(" + interpretPSql(sel) + ")"
+      case QueryNull => "NULL"
+    }
+
+    def reduceComparison(value: QueryComparison[_]): String = value match {
+      case QueryLit(v) => reduceValue(v)
+      case QueryEqual(left, QueryNull) => reduceValue(left) + " IS NULL"
+      case QueryEqual(left, right) => binOpReduction("=", left, right)(reduceValue)
+      case QueryNotEqual(left, QueryNull) => reduceValue(left) + " IS NOT NULL"
+      case QueryNotEqual(left, right) => binOpReduction("<>", left, right)(reduceValue)
+      case QueryGreaterThan(left, right) => binOpReduction(">", left, right)(reduceValue)
+      case QueryGreaterThanOrEqual(left, right) => binOpReduction(">=", left, right)(reduceValue)
+      case QueryIn(left, rights, _) => reduceValue(left) + " IN " + rights.map(reduceValue).mkString("(", ", ", ")")
+      case QueryLessThan(left, right) => binOpReduction("<", left, right)(reduceValue)
+      case QueryLessThanOrEqual(left, right) => binOpReduction("<=", left, right)(reduceValue)
+      case QueryAnd(left, right) => binOpReduction(" AND ", left, right)(reduceComparison)
+      case QueryOr(left, right) => binOpReduction(" OR ", left, right)(reduceComparison)
+      case QueryNot(v) => "!" + reduceComparison(v)
+    }
+
+    def reduceUnion(union: QueryUnion[_]): String = union match {
+      case QueryLeftOuterJoin(path, logic) => "LEFT OUTER JOIN " + reduceProjection(path) + " ON " + reduceComparison(logic)
+      case QueryRightOuterJoin(path, logic) => "RIGHT OUTER JOIN " + reduceProjection(path) + " ON " + reduceComparison(logic)
+      case QueryCrossJoin(path, logic) => "CROSS JOIN " + reduceProjection(path) + " ON " + reduceComparison(logic)
+      case QueryFullOuterJoin(path, logic) => "FULL OUTER JOIN " + reduceProjection(path) + " ON " + reduceComparison(logic)
+      case QueryInnerJoin(path, logic) => "INNER JOIN " + reduceProjection(path) + " ON " + reduceComparison(logic)
+    }
+
+    def reduceSort(sort: QuerySort): String = sort match {
+      case QuerySortAsc(path) => reducePath(path) + " ASC"
+      case QuerySortDesc(path) => reducePath(path) + " DESC"
+    }
+
+    def reduceInsertValues(insertValue: ModifyField[_]): (String, String) =
+      reducePath(insertValue.key) -> reduceValue(insertValue.value)
+
+    expr match {
+      case QuerySelect(table, values, unions, filters, sorts, groups, offset, limit, _) =>
+        val sqlProjections = values.map(reduceProjection).mkString(", ")
+        val sqlFilter = "WHERE " + reduceComparison(filters)
+        val sqlUnions = unions.map(reduceUnion).mkString(" ")
+        val sqlSorts =
+          if(sorts.isEmpty) ""
+          else "ORDER BY " + sorts.map(reduceSort).mkString(", ")
+
+        val sqlGroups =
+          if(groups.isEmpty) ""
+          else "GROUP BY " + groups.map(reduceSort).mkString(", ")
+
+        val sqlTable = reduceProjection(table)
+
+        val sqlOffset = offset.map("OFFSET " + _).getOrElse("")
+        val sqlLimit = limit.map("LIMIT " + _).getOrElse("")
+
+        s"SELECT $sqlProjections FROM $sqlTable $sqlUnions $sqlFilter $sqlSorts $sqlGroups $sqlLimit $sqlOffset".trim
+
 //      case QueryInsert(table, values) =>
 //        val sqlTable = reducePath(table)
 //        val mappedSqlValuesKV = values.map(reduceInsertValues)
@@ -107,7 +117,7 @@
 //        val sqlValues = mappedSqlValuesKV.map(_._2).mkString(", ")
 //
 //        s"INSERT INTO $sqlTable ($sqlColumns) VALUES ($sqlValues)"
-//
+
 //      case QueryUpdate(table, values, where) =>
 //        val sqlTable = reducePath(table)
 //        val mappedSqlValuesKV = values.map(reduceInsertValues).map(kv => kv._1 + "=" + kv._2).mkString(", ")
@@ -120,9 +130,9 @@
 //        val sqlWhere = reduceComparison(where)
 //
 //        s"DELETE $sqlTable WHERE $sqlWhere"
-//    }
-//
-//
-//
-//  }
-//}
+    }
+
+
+
+  }
+}
