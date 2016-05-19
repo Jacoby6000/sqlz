dc62f06f8b6d85d2dc7423b6361473206b09d201
Jacob Barber <Jacob.Barber@Bottlerocketstudios.com>
2016-05-19 17:44:16 -0500

    I'm an idiot. I haven't included the extensive test suite.

---
+++ postgres/src/test/scala/scoobie/dsl/weak/SqlTests.scala
@@ -0,0 +1,129 @@
+package scoobie.dsl.weak
+
+import scoobie.dsl.weak.sql._
+import scoobie.interpreters._
+import scoobie.interpreters.sqlDialects.postgres
+
+import doobie.imports._
+import org.specs2._
+import shapeless.syntax.singleton._
+
+import scalaz.NonEmptyList
+import scalaz.concurrent.Task
+
+
+
+/**
+  * Created by jbarber on 5/14/16.
+  */
+class SqlTests extends Specification {
+
+  val xa = DriverManagerTransactor[Task](
+    "org.postgresql.Driver", "jdbc:postgresql:world", "postgres", "postgres"
+  )
+
+  def is =
+    s2"""
+
+  Building queries should work properly
+    semi-complex select                               $semiComplexSelectResult
+    where in select                                   $selectWhereInResult
+    record life-cycle                                 ${endToEndTest.transact(xa).unsafePerformSync}
+                                                      """
+
+  case class Country(name: String, gnp: Int, code: String)
+
+  lazy val semiComplexSelect =
+    (
+      select(
+        p"c1.name",
+        (p"c1.gnp" + 5) as "c1gnp",
+        p"c1.code",
+        p"c2.name",
+        p"c2.gnp",
+        p"c2.code"
+      ) from (
+        p"country" as "c1"
+      ) innerJoin (
+        p"country" as "c2" on (
+          p"c2.code" === func"reverse" (p"c1.code")
+        )
+      ) where (
+        sql.not(p"c2.code" === "USA") and
+        p"c1.lifeexpectancy" > 50
+      )
+    ).build
+      .queryAndPrint[(Country, Country)](println _)
+      .list
+      .transact(xa)
+      .unsafePerformSync
+
+  lazy val selectWhereIn = {
+    val codes = NonEmptyList("TUV", "YUG")
+    implicit val codesParam = Param.many(codes)
+    (
+      select(p"name") from p"country" where (p"code" in ("USA", "BRA", codes.narrow))
+    ).build
+      .queryAndPrint[String](println _)
+      .list
+      .transact(xa)
+      .unsafePerformSync
+  }
+
+  lazy val endToEndTest = {
+    for {
+      inserted <- (insertInto(p"city") values(
+                    p"id" ==> 4080,
+                    p"name" ==> "test",
+                    p"countrycode" ==> "SHT",
+                    p"district" ==> "District of unlawful testing",
+                    p"population" ==> 1
+                  )).updateAndPrint(println).run
+
+      select1 <- (select(p"district") from p"city" where (p"id" === 4080))
+                    .build
+                    .queryAndPrint[String](println)
+                    .option
+
+      updated <- (update(p"city") set (p"population" ==> 10) where (p"id" === 4080))
+                    .build
+                    .updateAndPrint(println)
+                    .run
+
+      select2 <- (select(p"population") from p"city" where (p"id" === 4080))
+                    .build
+                    .queryAndPrint[Int](println)
+                    .option
+
+      deleted <- (deleteFrom(p"city") where (p"id" === 4080))
+                    .updateAndPrint(println)
+                    .run
+
+      select3 <- (select(p"population") from p"city" where (p"id" === 4080))
+                    .build
+                    .queryAndPrint[Int](println)
+                    .option
+    } yield {
+      inserted must beEqualTo(1)
+      select1  must beEqualTo(Some("District of unlawful testing"))
+      updated  must beEqualTo(1)
+      select2  must beEqualTo(Some(10))
+      deleted  must beEqualTo(1)
+      select3  must beEqualTo(None)
+    }
+  }
+
+//    id integer NO
+//    name varchar
+//    countrycode c
+//    district varc
+//    population in
+
+  def semiComplexSelectResult = {
+    semiComplexSelect must haveSize(10)
+  }
+
+  def selectWhereInResult = {
+    selectWhereIn must haveSize(4)
+  }
+}
