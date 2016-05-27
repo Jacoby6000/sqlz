89dbf3f1c49bd5b53abdc81a2386de3fa8bdd4f9
Jacob Barber <JacobA.Barber@yahoo.com>
2016-05-26 23:10:08 -0500

    update build script again

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "scoobie" ]; then
     sbt clean coverage test coverageReport tut
-    cmp readme.md doc/target/scala-2.11/tut/readme.md
+    cmp readme.md doc/target/scala-2.11/tut/readme.md && bash <(curl -s https://codecov.io/bash)
 else
     sbt $1/clean $1/test
 fi
