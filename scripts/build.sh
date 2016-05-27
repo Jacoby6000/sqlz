8b1334af139de578cec88b214a248f7a6e6b242b
Jacob Barber <JacobA.Barber@yahoo.com>
2016-05-26 23:08:39 -0500

    update build script

---
+++ scripts/build.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+if [ "$1" = "scoobie" ]; then
+    sbt clean coverage test coverageReport tut
+    cmp readme.md doc/target/scala-2.11/tut/readme.md
+else
+    sbt $1/clean $1/test
+fi
