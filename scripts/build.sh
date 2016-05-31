034036a48be690407acefc6f4baf41fd6dcf838d
Jacob Barber <Jacoby6000@gmail.com>
2016-05-31 12:45:44 -0500

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,5 @@ if [ "$1" = "scoobie" ]; then
     sbt clean coverage test coverageReport tut
     cmp readme.md doc/target/scala-2.11/tut/readme.md && bash <(curl -s https://codecov.io/bash)
 else
-    sbt $1/clean $1/coverage $1/clean $1/test  $1/coverageReport
+    sbt $1/clean coverage $1/test coverageReport
 fi
