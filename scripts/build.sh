b2be8c3a43a2d9566e58bcdd2598d279da0183a4
Jacob Barber <jacob.barber@stackpath.com>
2017-06-13 13:12:46 -0500

    coverage for integration test

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 if [ "$1" = "scoobie" ]; then
-    sbt clean coverage $2 coverageReport coverageAggregate tut
+    sbt clean coverage $2 $3 coverageReport coverageAggregate tut
     bash <(curl -s https://codecov.io/bash)
 else
     sbt $1/clean coverage $1/$2 coverageReport
