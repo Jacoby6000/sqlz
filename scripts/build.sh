d4a2abf94d9d2260a08bc5fce7b3605e292c584f
Jacob Barber <jacob.barber@stackpath.com>
2017-06-15 18:29:00 -0500

    prime gh-pages branch

--- scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-if [ "$1" = "scoobie" ]; then
-    sbt clean coverage $2 coverageReport $3 coverageReport coverageAggregate
-    bash <(curl -s https://codecov.io/bash)
-else
-    sbt $1/clean $1/$2
-fi
