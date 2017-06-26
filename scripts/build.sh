fadfe5309dc3c54f0c1475807a8cb6830344028c
Jacob Barber <jacob.barber@stackpath.com>
2017-06-26 10:31:20 -0500

    reduce number of vms travis needs for testing

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,3 @@
 #! /bin/bash
 
-export
-
-if [ "$1" = "scoobie" ]; then
-    sbt clean coverage $2 coverageReport $3 coverageReport coverageAggregate
-elif [ "$1" = "tut" ]; then
-    sbt docs/tut
-else
-    sbt $1/clean $1/$2
-fi
+sbt clean coverage test coverageReport it:test coverageReport coverageAggregate && sbt scoobieDoobie40/it:test && sbt scoobieDoobie41/it:test && sbt docs/tut
