d670edba64a7c9ba8bcd33f9f4d45879c219be19
Jacob Barber <jacob.barber@stackpath.com>
2017-06-26 08:47:03 -0500

    change coverage condition

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,6 @@
 
 if [ "$1" = "scoobie" ]; then
     sbt clean coverage $2 coverageReport $3 coverageReport coverageAggregate
-    bash <(curl -s https://codecov.io/bash)
 elif [ "$1" = "tut" ]; then
     sbt docs/tut
 else
