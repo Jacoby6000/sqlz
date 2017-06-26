08c166927e57fc22b120e12343017ff9e61973fc
Jacob Barber <jacob.barber@stackpath.com>
2017-06-26 10:49:46 -0500

    change order of sbt tests

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-
-sbt ++$TRAVIS_SCALA_VERSION clean coverage test coverageReport it:test coverageReport coverageAggregate && sbt ++$TRAVIS_SCALA_VERSION scoobieDoobie40/it:test && sbt ++$TRAVIS_SCALA_VERSION scoobieDoobie41/it:test && sbt ++$TRAVIS_SCALA_VERSION docs/tut
+sbt ++$TRAVIS_SCALA_VERSION scoobieDoobie40/it:test && sbt ++$TRAVIS_SCALA_VERSION scoobieDoobie41/it:test && sbt ++$TRAVIS_SCALA_VERSION docs/tut && sbt ++$TRAVIS_SCALA_VERSION clean coverage test coverageReport it:test coverageReport coverageAggregate
