c7a9b10c949167fe5ea233c6d5a20f5cd1075e0d
Jacob Barber <jacob.barber@stackpath.com>
2017-06-26 08:42:28 -0500

    add tut testing to travis builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,8 @@
 if [ "$1" = "scoobie" ]; then
     sbt clean coverage $2 coverageReport $3 coverageReport coverageAggregate
     bash <(curl -s https://codecov.io/bash)
+elif [ "$1" = "tut" ]; then
+    sbt docs/tut
 else
     sbt $1/clean $1/$2
 fi
