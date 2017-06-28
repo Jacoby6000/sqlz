b84449301273e2f6d88f8da13d86e6e07c53aa20
Jacob Barber <jacob.barber@stackpath.com>
2017-06-28 15:26:42 -0500

    This feels wrong.... Mostly the QueryExpressions.

--- core/src/main/scala/scoobie/ast.scala
+++ core/src/main/scala/scoobie/ast.scala
@@ -15,18 +15,14 @@ object ast {
     }
   }
 
-  sealed trait QueryValue[F[_]]
-  case class QueryRawExpression[F[_], T](t: T)(implicit val rawExpressionHandler: RawExpressionHandler[T]) extends QueryValue[F]
-  case class QueryParameter[F[_], T](value: T)(implicit val ev: F[T]) extends QueryValue[F] {
-    type Out = T
-  }
-
-  case class QueryFunction[F[_]] private (path: QueryPath[F], args: List[QueryValue[F]]) extends QueryValue[F]
-  case class QueryAdd[F[_]] private (left: QueryValue[F], right: QueryValue[F]) extends QueryValue[F]
-  case class QuerySub[F[_]] private (left: QueryValue[F], right: QueryValue[F]) extends QueryValue[F]
-  case class QueryDiv[F[_]] private (left: QueryValue[F], right: QueryValue[F]) extends QueryValue[F]
-  case class QueryMul[F[_]] private (left: QueryValue[F], right: QueryValue[F]) extends QueryValue[F]
-  class QueryNull[F[_]] extends QueryValue[F] {
+  sealed trait QueryValue[F[_], A]
+  case class QueryParameter[F[_], A, T](value: T)(implicit val ev: F[T]) extends QueryValue[F, A]
+  case class QueryFunction[F[_], A] private (path: QueryPath[F, A], args: List[A]) extends QueryValue[F, A]
+  case class QueryAdd[F[_], A] private (left: A, right: A) extends QueryValue[F, A]
+  case class QuerySub[F[_], A] private (left: A, right: A) extends QueryValue[F, A]
+  case class QueryDiv[F[_], A] private (left: A, right: A) extends QueryValue[F, A]
+  case class QueryMul[F[_], A] private (left: A, right: A) extends QueryValue[F, A]
+  class QueryNull[F[_], A] extends QueryValue[F, A] {
     override def equals(obj: scala.Any): Boolean = obj match {
       case _: QueryNull[_] => true
       case _ => false
@@ -36,40 +32,40 @@ object ast {
   }
 
   object QueryNull {
-    def apply[F[_]]: QueryNull[F] = new QueryNull[F]
+    def apply[F[_], A]: QueryNull[F, A] = new QueryNull[F]
   }
 
-  sealed trait QueryComparison[F[_]]
-  class QueryComparisonNop[F[_]] extends QueryComparison[F] {
+  sealed trait QueryComparison[F[_], G[_], A]
+  class QueryComparisonNop[F[_], G[_], A] extends QueryComparison[F, G, A] {
     override def equals(obj: scala.Any): Boolean = obj match {
-      case _: QueryComparisonNop[_] => true
+      case _: QueryComparisonNop[_, _, _] => true
       case _ => false
     }
 
     override def toString: String = "QueryComparisonNop"
   }
   object QueryComparisonNop {
-    def apply[F[_]]: QueryComparisonNop[F] = new QueryComparisonNop[F]
+    def apply[F[_], G[_], A]: QueryComparisonNop[F, G, A] = new QueryComparisonNop[F, G, A]
   }
 
-  case class QueryEqual[F[_]](left: QueryValue[F], right: QueryValue[F]) extends QueryComparison[F]
-  case class QueryGreaterThan[F[_]](left: QueryValue[F], right: QueryValue[F]) extends QueryComparison[F]
-  case class QueryGreaterThanOrEqual[F[_]](left: QueryValue[F], right: QueryValue[F]) extends QueryComparison[F]
-  case class QueryLessThan[F[_]](left: QueryValue[F], right: QueryValue[F]) extends QueryComparison[F]
-  case class QueryLessThanOrEqual[F[_]](left: QueryValue[F], right: QueryValue[F]) extends QueryComparison[F]
-  case class QueryAnd[F[_]](left: QueryComparison[F], right: QueryComparison[F]) extends QueryComparison[F]
-  case class QueryOr[F[_]](left: QueryComparison[F], right: QueryComparison[F]) extends QueryComparison[F]
-  case class QueryIn[F[_]](left: QueryValue[F], rights: List[QueryValue[F]]) extends QueryComparison[F]
-  case class QueryLit[F[_]](value: QueryValue[F]) extends QueryComparison[F]
-  case class QueryNot[F[_]](value: QueryComparison[F]) extends QueryComparison[F]
+  case class QueryEqual[F[_], A, B](left: B, right: B)(implicit val ev: F[B]) extends QueryComparison[F, A]
+  case class QueryGreaterThan[F[_], A, B](left: B, right: B)(implicit val ev: F[B]) extends QueryComparison[F, A]
+  case class QueryGreaterThanOrEqual[F[_], A, B](left: B, right: B)(implicit val ev: F[B]) extends QueryComparison[F, A]
+  case class QueryLessThan[F[_], A, B](left: B, right: B)(implicit val ev: F[B]) extends QueryComparison[F, A]
+  case class QueryLessThanOrEqual[F[_], A, B](left: B, right: B)(implicit val ev: F[B]) extends QueryComparison[F, A]
+  case class QueryIn[F[_], A, B](left: B, rights: List[B])(implicit val ev: F[B]) extends QueryComparison[F, A]
+  case class QueryLit[F[_], A, B](value: B)(implicit val ev: F[B]) extends QueryComparison[F, A]
+  case class QueryAnd[F[_], G[_], A](left: A, right: A) extends QueryComparison[F, A]
+  case class QueryOr[F[_], G[_], A](left: A, right: A) extends QueryComparison[F, A]
+  case class QueryNot[F[_], G[_], A](value: A) extends QueryComparison[F, A]
 
-  sealed trait QueryPath[F[_]] extends QueryValue[F]
-  case class QueryPathEnd[F[_]](path: String) extends QueryPath[F] with QueryValue[F]
-  case class QueryPathCons[F[_]](path: String, queryPath: QueryPath[F]) extends QueryPath[F] with QueryValue[F]
+  sealed trait QueryPath[F[_], A] extends QueryValue[F, A]
+  case class QueryPathEnd[F[_], A](path: String) extends QueryPath[F, A] with QueryValue[F, A]
+  case class QueryPathCons[F[_], A](path: String, queryPath: QueryPath[F, A]) extends QueryPath[F, A] with QueryValue[F, A]
 
-  sealed trait QueryProjection[F[_]]
-  case class QueryProjectOne[F[_]](selection: QueryValue[F], alias: Option[String]) extends QueryProjection[F]
-  class QueryProjectAll[F[_]] extends QueryProjection[F] {
+  sealed trait QueryProjection[A]
+  case class QueryProjectOne[A](selection: A, alias: Option[String]) extends QueryProjection[A]
+  class QueryProjectAll[A] extends QueryProjection[A] {
     override def equals(obj: scala.Any): Boolean = obj match {
       case _: QueryProjectAll[_] => true
       case _ => false
@@ -79,38 +75,38 @@ object ast {
   }
 
   object QueryProjectAll {
-    def apply[F[_]]: QueryProjectAll[F] = new QueryProjectAll[F]
+    def apply[A]: QueryProjectAll[A] = new QueryProjectAll[A]
   }
 
 
-  sealed trait QueryJoin[F[_]]
-  case class QueryInnerJoin[F[_]](table: QueryProjection[F], on: QueryComparison[F]) extends QueryJoin[F]
-  case class QueryFullOuterJoin[F[_]](table: QueryProjection[F], on: QueryComparison[F]) extends QueryJoin[F]
-  case class QueryLeftOuterJoin[F[_]](table: QueryProjection[F], on: QueryComparison[F]) extends QueryJoin[F]
-  case class QueryRightOuterJoin[F[_]](table: QueryProjection[F], on: QueryComparison[F]) extends QueryJoin[F]
-  case class QueryCrossJoin[F[_]](table: QueryProjection[F], on: QueryComparison[F]) extends QueryJoin[F]
+  sealed trait QueryJoin[A, B]
+  case class QueryInnerJoin[A, B](table: A, on: B) extends QueryJoin[A, B]
+  case class QueryFullOuterJoin[A, B](table: A, on: B) extends QueryJoin[A, B]
+  case class QueryLeftOuterJoin[A, B](table: A, on: B) extends QueryJoin[A, B]
+  case class QueryRightOuterJoin[A, B](table: A, on: B) extends QueryJoin[A, B]
+  case class QueryCrossJoin[A, B](table: A, on: B) extends QueryJoin[A, B]
 
-  sealed trait QuerySort[F[_]]
-  case class QuerySortAsc[F[_]](path: QueryPath[F]) extends QuerySort[F]
-  case class QuerySortDesc[F[_]](path: QueryPath[F]) extends QuerySort[F]
+  sealed trait QuerySort[A]
+  case class QuerySortAsc[A](path: A) extends QuerySort[A]
+  case class QuerySortDesc[A](path: A) extends QuerySort[A]
 
-  sealed trait QueryExpression[F[_]]
-  sealed trait QueryModify[F[_]] extends QueryExpression[F]
+  sealed trait QueryExpression[F[_], G[_]]
+  sealed trait QueryModify[F[_], G[_]] extends QueryExpression[F, G]
 
-  case class QuerySelect[F[_]](
-                                table: QueryProjection[F],
-                                values: List[QueryProjection[F]],
-                                unions: List[QueryJoin[F]],
-                                filter: QueryComparison[F],
-                                sorts: List[QuerySort[F]],
-                                groupings: List[QuerySort[F]],
+  case class QuerySelect[F[_], G[_], A, B, C, D, E](
+                                table: QueryProjection[A],
+                                values: List[QueryProjection[A]],
+                                unions: List[QueryJoin[A, B]],
+                                filter: QueryComparison[G, C],
+                                sorts: List[QuerySort[D]],
+                                groupings: List[QuerySort[D]],
                                 offset: Option[Long],
                                 limit: Option[Long]
-  ) extends QueryExpression[F] with QueryValue[F]
+  ) extends QueryExpression[F, G] with QueryValue[F, E]
 
-  case class ModifyField[F[_]](key: QueryPath[F], value: QueryValue[F])
-  case class QueryInsert[F[_]](collection: QueryPath[F], values: List[ModifyField[F]]) extends QueryExpression[F] with QueryModify[F]
-  case class QueryUpdate[F[_]] private (collection: QueryPath[F], values: List[ModifyField[F]], where: QueryComparison[F]) extends QueryExpression[F] with QueryModify[F]
-  case class QueryDelete[F[_]](collection: QueryPath[F], where: QueryComparison[F]) extends QueryExpression[F] with QueryModify[F]
+  case class ModifyField[K, V](key: K, value: V)
+  case class QueryInsert[F[_], G[_], A, K, V](collection: QueryPath[F, A], values: List[ModifyField[K, V]]) extends QueryExpression[F, G] with QueryModify[F, G]
+  case class QueryUpdate[F[_], G[_], A, B, K, V](collection: QueryPath[F, A], values: List[ModifyField[K, V]], where: QueryComparison[G, B]) extends QueryExpression[F, G] with QueryModify[F, G]
+  case class QueryDelete[F[_], G[_], A, B](collection: QueryPath[F, A], where: QueryComparison[G, B]) extends QueryExpression[F, G] with QueryModify[F, G]
 
 }
